a324b7a5ddff5ec462ff429de4745019beadbe49
Mingxuan Liu <mingxuan.liu@emory.edu>
2023-10-14 16:49:36 -0400

    Implemented the code to minimize the absolute errors between the two arrays.

--- optimizer.py
+++ optimizer.py
@@ -3,17 +3,16 @@ from scipy.optimize import minimize
 from particle_helper import crop_particle
 
 
-def abs_error(theta, particle, cropped_image, domain_size, length, width):
+def abs_error(theta, particle, cropped_image, domain_size, boundary):
     """
     Calculate the absolute error between the experimental image and the particle model.
 
     Parameters:
     - theta: Rotation angle.
     - particle: The particle model.
-    - experimental_image: The experimental image data.
-    - config_name: The configuration name for creating the particle.
+    - cropped_image: The cropped experimental image data.
     - domain_size: Side length of the squared domain for shadow generation.
-    - crop_size: Tuple (height, width) defining the size of the cropped area around the particle center.
+    - boundary: Tuple (height, width) defining the size of the cropped area around the particle center.
 
     Returns:
     - The absolute error between the experimental image and the particle model.
@@ -28,35 +27,43 @@ def abs_error(theta, particle, cropped_image, domain_size, length, width):
     shadow_arr = particle.shadow('xz', domain_size)
 
     # Crop the shadow around the center
-    cropped_particle = crop_particle(shadow_arr, length, width)
+    cropped_particle = crop_particle(shadow_arr, boundary[0], boundary[1])
 
     # Calculate and return the absolute error
     return np.sum(np.abs(cropped_image - cropped_particle))
 
 
-def optimize_rotation_angle(initial_guess, particle, cropped_image, domain_size, length, width):
+def optimize_rotation_angle(particle, cropped_image, domain_size, boundary, initial_guess, search_range=20):
     """
     Find the rotation angle 'theta' that minimizes the absolute error between the .tif image and the particle model.
 
     Parameters:
-    - initial_theta: Initial guess for the rotation angle.
     - particle: The particle model.
-    - experimental_image: The experimental image data.
-    - config_name: The configuration name for creating the particle.
+    - cropped_image: The experimental image data.
     - domain_size: Side length of the squared domain for shadow generation.
-    - crop_size: Tuple (height, width) defining the size of the cropped area around the particle center.
+    - boundary: Tuple (height, width) defining the size of the cropped area around the particle center.
+    - initial_guess: Initial guess for the rotation angle.
+    - search_range: Range to search around the initial guess (default is 5).
 
     Returns:
     - Optimal rotation angle.
     """
-    # Minimize the absolute error
-    result = minimize(
-        abs_error,  # function to minimize
-        initial_guess,  # initial guess for the theta value
-        args=(particle, cropped_image, domain_size, length, width),
-        # additional arguments for `absolute_error`
-        bounds=[(0, 180)]  # bounds for theta
-    )
+    min_error = float('inf')  # Initialize minimum error as infinity
+    optimal_theta = initial_guess  # Initialize optimal theta as the initial guess
+
+    # Define the search bounds
+    lower_bound = max(0, initial_guess - search_range)
+    upper_bound = min(180, initial_guess + search_range)
+
+    # Loop through possible theta values within the search bounds
+    for theta in range(lower_bound, upper_bound + 1):
+        # Calculate the absolute error for the current theta
+        error = abs_error(theta, particle, cropped_image, domain_size, boundary)
+
+        # If the error is smaller than the current minimum, update min_error and optimal_theta
+        if error < min_error:
+            min_error = error
+            optimal_theta = theta
 
     # Return the optimal rotation angle
-    return result.x[0]
+    return optimal_theta
